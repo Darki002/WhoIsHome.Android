5e427c19279da09029e269a9133b01052ce1b41b
Llyn Baumann <darkizockt16@gmail.com>
2024-02-22 10:12:37 +0100

    upgrade everything

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 buildscript{
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+        classpath("com.google.gms:google-services:4.4.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
     }
 }
