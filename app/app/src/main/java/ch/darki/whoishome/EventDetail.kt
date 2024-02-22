1e756277facf4dd4f50403bd6310e045d4c91d61
Llyn Baumann <darkizockt16@gmail.com>
2024-02-22 09:17:53 +0100

    detail view window set up

--- app/app/src/main/java/ch/darki/whoishome/EventDetail.kt
+++ app/app/src/main/java/ch/darki/whoishome/EventDetail.kt
@@ -39,7 +39,31 @@ class EventDetail : Fragment() {
             return
         }
 
-        // TODO: Set up the detail window
+        view?.findViewById<TextView>(R.id.event_detail_title)?.text = event.eventName
+
+        val date = getString(R.string.datum_1_s, event.date.toString("dd.MM.yyyy"))
+        view?.findViewById<TextView>(R.id.event_detail_date)?.text = date
+
+        val startTime = event.startTime.toString("HH:mm")
+        val endTime = event.endTime.toString("HH:mm")
+        val duration = getString(R.string.event_duration_format, startTime, endTime)
+        view?.findViewById<TextView>(R.id.event_detail_duration)?.text = duration
+
+        val dinnerDetailView = view?.findViewById<TextView>(R.id.dinner_detail_info)
+        if(!event.relevantForDinner){
+            dinnerDetailView?.text = getString(R.string.event_not_relevant_for_dinner)
+        }
+        else {
+            val dinnerAt = event.dinnerAt?.toString("HH:mm")
+
+            if (dinnerAt == null) {
+                dinnerDetailView?.text = getString(R.string.not_at_home_for_dinner)
+            }
+            else{
+                val dinnerInfo = getString(R.string.event_relevant_for_dinner, dinnerAt)
+                dinnerDetailView?.text = dinnerInfo
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
