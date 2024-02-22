69cbe712feb5ce30dec2c973607243c6729c4f89
Llyn Baumann <darkizockt16@gmail.com>
2024-02-22 08:44:15 +0100

    action and param for fragment

--- app/app/src/main/java/ch/darki/whoishome/EventDetail.kt
+++ app/app/src/main/java/ch/darki/whoishome/EventDetail.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import ch.darki.whoishome.core.Event
 
 class EventDetail : Fragment() {
 
@@ -20,7 +22,24 @@ class EventDetail : Fragment() {
         val fragment = inflater.inflate(R.layout.fragment_event_detail, container, false)
         service = activity?.applicationContext as ServiceManager
 
+        val args: EventDetailArgs = EventDetailArgs.fromBundle(requireArguments())
+        showDetails(args.eventId)
+
         return fragment
+    }
+
+    private fun showDetails(eventId: String){
+        service.presenceService.eventService.getEventById(eventId, viewLifecycleOwner.lifecycleScope) {
+            setUpDetailWindow(it)
+        }
+    }
+
+    private fun setUpDetailWindow(event : Event?){
+        if(event == null){
+            return
+        }
+
+        // TODO: Set up the detail window
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
