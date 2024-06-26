2ce8c6299a3a2aae63489f0af7b07150ecbfb727
Darki002 <darkizockt16@gmail.com>
2024-06-26 16:45:42 +0200

    Delete InspectPersonViewModel.kt

--- app/app/src/main/java/ch/darki/whoishome/InspectPersonViewModel.kt
+++
@@ -1,8 +0,0 @@
-package ch.darki.whoishome
-
-import androidx.lifecycle.ViewModel
-import ch.darki.whoishome.core.models.Person
-
-class InspectPersonViewModel : ViewModel() {
-    var person: Person? = null
-}
