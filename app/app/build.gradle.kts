8e6fd623e6ca2c7896b1a6d409d79ae4ad95d4b0
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-07-02 20:42:06 +0200

    3.3

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ch.darki.whoishome"
         minSdk = 31
         targetSdk = 34
-        versionCode = 5
-        versionName = "3.2"
+        versionCode = 6
+        versionName = "3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
